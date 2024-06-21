0d1505e748b1b9b344883c30c08736fc734cd255
Khalid <155519253+khalidh1b@users.noreply.github.com>
2024-06-21 09:35:54 +0600

    added carts in products section

--- src/Components/AllProducts/AllProducts.jsx
+++ src/Components/AllProducts/AllProducts.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import Product from "./Product";
 
 
 const AllProducts = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
+
     return (
         <div className="pt-28">
             <div className="flex text-[#DB4444] items-center gap-3 ml-36 font-semibold"><div className="bg-[#DB4444] rounded py-5 px-2"></div>Todays</div>
@@ -36,6 +46,14 @@ const AllProducts = () => {
                     <FaArrowLeftLong className="bg-[#F5F5F5] text-4xl p-2.5 rounded-full"/>
                 </div>
             </div>
+
+
+            {/* all products here */}
+            
+                <div className="flex justify-center gap-4 items-center pt-8">
+                    {products.map(product => <Product key={product.id} product={product}></Product>)}
+                </div>
+                <div className="flex justify-center pt-16"><button className="text-[#FAFAFA] text-base font-medium bg-[#DB4444] py-3 px-9 rounded">View All Products</button></div>
         </div>
     );
 };
---
+++ src/Components/AllProducts/Product.jsx
@@ -0,0 +1,45 @@
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+import { IoEyeOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa6";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+const Product = ({product}) => {
+    const {discount_percent, product_image, product_title, main_price, discount_price, rating, user_rating_count} = product;
+    
+    const [ratings, setRatings] = useState(rating);
+    return (
+        <div>
+            <div>
+                <div className="relative">
+                <img className="bg-[#F5F5F5] px-16 pt-16 pb-20 w-[300px] h-[300px] rounded" src={product_image} alt="G92 Gamepad" />
+                <span className="bg-[#DB4444] absolute top-5 left-4 text-white py-1 px-4 rounded poppins">{discount_percent}</span>
+                <IoEyeOutline className="bg-[#FFFFFF] absolute top-20 left-60 text-[45px] p-2.5 rounded-full"/>
+                <FaRegHeart className="bg-[#FFFFFF] absolute top-3 left-60 text-[45px] p-2.5 rounded-full"/>
+                <p className="bg-[#000000] absolute bottom-0 w-full text-base poppins font-medium text-[#FFFFFF] py-2.5 text-center rounded-b">Add To Cart</p>
+                </div>
+                <h4 className="text-[#000000] text-xl poppins font-semibold pt-3">{product_title}</h4>
+                <div className="flex gap-4 py-2"><h5 className="text-[#DB4444] text-xl font-medium">{discount_price}</h5><span className="text-gray-500 font-medium line-through text-xl">{main_price}</span></div>
+                <div className="flex gap-2 items-center"><Rating style={{ maxWidth: 130 }} value={ratings} onChange={setRatings}  isRequired/> <span className="text-gray-500 font-semibold text-[18px]">({user_rating_count})</span></div>
+            </div>
+        </div>
+    );
+};
+
+
+Product.propTypes = {
+    product: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        discount_percent: PropTypes.string.isRequired,
+        product_image: PropTypes.string.isRequired,
+        product_title: PropTypes.string.isRequired,
+        main_price: PropTypes.string.isRequired,
+        discount_price: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        user_rating_count: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default Product;
