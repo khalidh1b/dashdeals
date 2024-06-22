85d78e3965b4fa125fd13eeda3ce0cd6ccc87bff
Khalid <155519253+khalidh1b@users.noreply.github.com>
2024-06-22 07:43:50 +0600

    added a simple error page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,11 +3,13 @@ import Root from '../pages/Root/Root';
 import HomePage from '../pages/HomePage/HomePage';
 import Signup from "../pages/Signup/Signup";
 import Login from "../pages/Login/Login";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
