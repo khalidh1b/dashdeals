6961b145c1cc3ef3ebd45bb6fb67d8c506ca8b01
Khalid <155519253+khalidh1b@users.noreply.github.com>
2024-06-24 08:02:29 +0600

    added signin  feature in login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,37 @@
+import { useContext } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
+    const {signIn, googleSignIn} = useContext(AuthContext);
+
+    const handleSignIn = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const data = {email, password};
+        console.log(data);
+
+        signIn(data.email, data.password)
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
     return (
         <div className="flex items-center pb-32 pt-20 gap-24">
             <div className="bg-[#CBE4E8] pt-10">
@@ -9,14 +40,14 @@ const Login = () => {
             <div>
                 <h3 className="text-[#000] text-[36px] font-medium">Log in to Exclusive</h3>
                 <h5 className="text-[#000] poppins text-base font-normal pb-10">Enter your details below</h5>
-                <form>
-                    <input className="focus:outline-none border-b-2 w-full pb-1 mb-7 border-x-0 border-t-0" type="email" name="" id="" placeholder="Email or Phone Number"/> <br />
-                    <input className="border-b-2 focus:outline-none w-full  pb-1 border-x-0 border-t-0" type="password" name="" id="" placeholder="Password"/>
+                <form onSubmit={handleSignIn}>
+                    <input className="focus:outline-none border-b-2 w-full pb-1 mb-7 border-x-0 border-t-0" type="email" name="email" id="" placeholder="Email or Phone Number"/> <br />
+                    <input className="border-b-2 focus:outline-none w-full  pb-1 border-x-0 border-t-0" type="password" name="password" id="" placeholder="Password"/>
                     <p className="mb-8 pt-1 text-[#DB4444] text-[14px]">forget password </p>
                     <br />
                     <button className="text-white bg-[#DB4444] rounded py-3 w-full" type="submit">Login</button>
                 </form>
-                <div className="flex items-center gap-2 justify-center py-3 w-full mt-4 rounded text-[#000] poppins text-base font-normal cursor-pointer border-2"><FcGoogle className="text-2xl"/><span>Sign In with Google</span></div>
+                <div onClick={handleGoogleSignIn} className="flex items-center gap-2 justify-center py-3 w-full mt-4 rounded text-[#000] poppins text-base font-normal cursor-pointer border-2"><FcGoogle className="text-2xl"/><span>Sign In with Google</span></div>
                 <div className="flex justify-center gap-2 pt-8"><p className="poppins text-base font-normal text-[#000]">New to here go?</p><span className="text-[#000] text-base font-medium poppins">Signup</span></div>
             </div>
         </div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,25 +1,43 @@
-import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithPopup} from 'firebase/auth';
+import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup} from 'firebase/auth';
 import app from '../firebase/firebase.config';
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
     const googleProvider = new GoogleAuthProvider();
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
     const googleSignIn = () => {
         return signInWithPopup(auth, googleProvider)
     }
 
+    useEffect(() => {
+        const onSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        
+        console.log('current user:', currentUser);
+        })
+        return () => {
+            onSubscribe();
+        }
+    }, [])
+
     const authInfo = {
         createUser,
         googleSignIn,
+        user,
+        signIn,
     }
     return (
         <AuthContext.Provider value={authInfo}>
