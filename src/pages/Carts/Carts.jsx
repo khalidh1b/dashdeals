aebaefa026a8948911e7efb66dfcf8b7c58f9120
Khalid <155519253+khalidh1b@users.noreply.github.com>
2024-06-25 11:36:19 +0600

    added api for getting data for carts

--- src/pages/Carts/Carts.jsx
+++ src/pages/Carts/Carts.jsx
@@ -1,20 +1,44 @@
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 import { LuDelete } from "react-icons/lu";
-
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import {useQuery} from '@tanstack/react-query';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Carts = () => {
+    const axiosSecure = useAxiosSecure();
+    const {user} = useContext(AuthContext);
+    console.log(user)
+
+    const {data: products = [], refetch} = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/userProductCarts/${user.email}`, {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('access-token')}`
+                }
+            })
+            return res.data;
+        }
+    })
+
+    // const handleDelete = (product_id) => {
+    //     console.log('handle delete', product_id)
+    //     axiosSecure.delete('/')
+    }
     return (
         <div className="pt-24 pb-32">
             <div className="flex justify-center gap-72 border mx-32 py-5 px-5 rounded shadow-md text-base poppins font-normal"><p className="w-1/2">Product</p><p>Price</p><p>Quantity</p><p>Subtotal</p></div>
 
-                <div className="flex items-center justify-center border mx-32 mt-9 py-7 rounded shadow-md text-base poppins font-normal px-5">
+                {
+                    products.map(product => <div key={product._id} className="flex items-center justify-center border mx-32 mt-9 py-7 rounded shadow-md text-base poppins font-normal px-5">
                     <div className="flex items-center gap-4 w-4/12 relative">
-                        <LuDelete className="absolute text-xl text-red-500 bottom-7 left-9  top-"/>
-                        <img className="w-12 h-10" src="https://i.postimg.cc/x8hr0VH9/G92-Gamepad.png" alt="#" />
-                        <span>H1 Gamepad iron man diagram</span>
+                        <LuDelete onClick={() => handleDelete(product._id)} className="absolute text-xl text-red-500 bottom-7 left-9 top- cursor-pointer"/>
+                        <img className="w-12 h-10" src={product.product_image} alt="#" />
+                        <span>{product.product_title}</span>
                     </div>
-                    <p className="w-3/12">$650</p>
+                    <p className="w-3/12">{product.discount_price}</p>
                     <div className="w-2/12 pl-10">
                         <div className="flex items-center justify-center gap-4 py-1 rounded border-[#000] w-20 border">
                             <span>01</span>
@@ -22,25 +46,10 @@ const Carts = () => {
                             <IoIosArrowDown /></div>
                         </div>
                     </div>
-                    <p className="w-3/12 flex justify-end pr-3">$650</p>
-                </div>
+                    <p className="w-3/12 flex justify-end pr-3">{product.discount_price}</p>
+                </div>)
+                }
 
-                <div className="flex items-center justify-center border mx-32 mt-9 py-7 rounded shadow-md text-base poppins font-normal px-5">
-                    <div className="flex items-center gap-4 w-4/12 relative">
-                        <LuDelete className="absolute text-xl text-red-500 bottom-7 left-9  top-"/>
-                        <img className="w-12 h-10" src="https://i.postimg.cc/x8hr0VH9/G92-Gamepad.png" alt="#" />
-                        <span>H1 Gamepad iron man diagram</span>
-                    </div>
-                    <p className="w-3/12">$650</p>
-                    <div className="w-2/12 pl-10">
-                        <div className="flex items-center justify-center gap-4 py-1 rounded border-[#000] w-20 border">
-                            <span>01</span>
-                            <div><IoIosArrowUp />
-                            <IoIosArrowDown /></div>
-                        </div>
-                    </div>
-                    <p className="w-3/12 flex justify-end pr-3">$650</p>
-                </div>
 
             <div className="flex justify-between mx-32 pt-8">
                 <button className="text-[#000] poppins text-base font-medium py-3 px-9 border-2 rounded">Return To Shop</button>
