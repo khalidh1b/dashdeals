afec636615891add40b4f3f6dd3eaeb881238f4c
Khalid <155519253+khalidh1b@users.noreply.github.com>
2024-06-28 20:49:33 +0600

    added ssl commerz payment gateway system

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const {v4: uuidv4} = require('uuid');
 
 //middlewares
 app.use(cors());
+app.use(express.urlencoded());
 app.use(express.json());
 
 
 //mongodb
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { default: axios } = require('axios');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rpbygkt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,6 +37,8 @@ async function run() {
         const userProductWishlistCollections = client.db('Dashdeals').collection('userProductWishlist');
         const userProductCartCollections = client.db('Dashdeals').collection('userProductCarts');
         const userFeedbackMessageCollections = client.db('Dashdeals').collection('userFeedbackMessage');
+        const paymentDataCheckoutCollections = client.db('Dashdeals').collection('paymentDataCheckout');
+        const userPaymentsInfoCollections = client.db('Dashdeals').collection('userPaymentsInfo');
 
         //jwt related apis
         app.post('/jwt', async(req, res) => {
@@ -87,9 +92,15 @@ async function run() {
             res.send(result);
         })
 
-        app.post('/userProductCarts', async(req, res) => {
+        app.post('/userProductCarts/:id/:email', async(req, res) => {
             const data = req.body;
             console.log(data)
+            const {id, email} = req.params;
+            const query = {email: email, _id: (id)};
+            const checkCart = await userProductCartCollections.findOne(query);
+            if(checkCart) {
+                return res.send({message: 'This Product already saved in Carts', insertedId: null});
+            }
             const result = await userProductCartCollections.insertOne(data);
             res.send(result);
         })
@@ -113,6 +124,102 @@ async function run() {
             const data = req.body;
             const result = await userFeedbackMessageCollections.insertOne(data);
             res.send(result);
+        })
+
+        app.post('/sendDataCheckout', async(req, res) => {
+            const data = req.body;
+            const result = await paymentDataCheckoutCollections.insertOne(data);
+            res.send(result);
+        })
+
+        //payment gateway integration
+        app.post('/create-payment', async(req, res) => {
+            const paymentInfo = req.body;
+            const initiateData = {
+                store_id: 'xcorp667d3ec10ce8f',
+                store_passwd: 'xcorp667d3ec10ce8f@ssl',
+                total_amount: paymentInfo.amount,
+                currency: paymentInfo.currency,
+                tran_id: uuidv4(),
+                success_url: 'http://localhost:5000/success-payment',
+                fail_url: 'http://localhost:5000/payment-failed',
+                cancel_url: 'http://localhost:5000/payment-cancel',
+                cus_name: 'Customer Name',
+                cus_email: 'cust@yahoo.com',
+                cus_add1: 'Dhaka',
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: '01711111111',
+                cus_fax: '01711111111',
+                shipping_method: 'NO',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: '1000',
+                ship_country: 'Bangladesh',
+                product_name: 'Laptop',
+                product_category: 'Laptop',
+                product_profile: 'general',
+                multi_card_name: 'mastercard,visacard,amexcard',
+                value_a: 'ref001_A',
+                value_b: 'ref002_B',
+                value_c: 'ref003_C',
+                value_d: 'ref004_D'
+            }
+
+            const responce = await axios({
+                method: 'POST',
+                url: 'https://sandbox.sslcommerz.com/gwprocess/v4/api.php',
+                data: initiateData,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+            console.log(responce);
+
+            const saveData = {
+                cus_name: 'dummy',
+                paymentId: initiateData.tran_id,
+                ammount: paymentInfo.ammount,
+                status: 'pending'
+            }
+
+            const saved = await userPaymentsInfoCollections.insertOne(saveData);
+            if(saved) {
+                res.send({
+                    paymentUrl: responce.data.GatewayPageURL,
+                });
+            }
+        })
+
+        app.post('/success-payment', async(req, res) => {
+            const successData = req.body;
+            if(successData.status !== 'VALID') {
+                throw new Error('unauthorized payment');
+            }
+
+            const query = { paymentId: successData.tran_id }
+            const update = {
+                $set: {
+                    status: "Success"
+                }
+            }
+            const updateData = await userPaymentsInfoCollections.updateOne(query, update);
+
+            console.log('successData', successData);
+            console.log('updatedData', updateData);
+            res.redirect('http://localhost:5173/paymentsuccess')
+        })
+        app.post('/payment-failed', async(req, res) => {
+            res.redirect('http://localhost:5173/paymentfailed');
+        })
+        app.post('/payment-cancel', async(req, res) => {
+            res.redirect('http://localhost:5173/paymentcancel');
         })
 
         // Send a ping to confirm a successful connection
