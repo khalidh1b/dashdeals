675719b9648046ca51be274a0ad94e82e57a9471
Khalid <155519253+khalidh1b@users.noreply.github.com>
2024-08-22 10:21:27 +0600

    Added API to fetch user-ordered products, delete ordered products, delete user products wishlist, get user products wishlist by email and its product ids with error handling.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const {client} = require('../config/db.js');
+const {ObjectId} = require('mongodb');
 
 const getCollection = (collectionName) => client.db('Dashdeals').collection(collectionName);
 
@@ -73,5 +74,69 @@ exports.deleteUserProductCarts = async (req, res) => {
         res.status(500).send({message: 'An error occurred while deleting item from cart'});
     }
 };
+
+exports.getUserOrderedProducts = async (req, res) => {
+    try {
+        const email = req.params.email;
+        const query = {cus_email: email};
+
+        const orders = await getCollection('userOrderedProducts').find(query).toArray();
+        if(orders) {
+            res.json(orders);
+        }
+        else {
+            res.status(404).json({message: 'No orders found for this email'});
+        }
+    } catch (error) {
+        console.error('Error fetching user ordered products', error);
+        res.status(500).json({message: 'An error occurred fetching user ordered products'});
+    }
+}
+
+exports.deleteOrderedProduct = async (req, res) => {
+    try {
+        const {orderId, productId} = req.params;
+        const query =  {_id: new ObjectId(orderId)}
+        console.log(orderId, productId, query);
+
+        const order = await getCollection('userOrderedProducts').findOne(query);
+        console.log(order);
+        
+        const updatedProducts = await order.products.filter((product) => product._id !== (productId));
+
+        if(updatedProducts.length === 0) {
+            console.log('this order should be delete',order);
+            const result = await getCollection('userOrderedProducts').deleteOne(query);
+            console.log('order deleted:', result);
+        }
+
+        await getCollection('userOrderedProducts').updateOne(
+            { _id: new ObjectId(orderId) },
+            { $set: { products: updatedProducts } }
+        );
+
+        res.status(200).json({message: 'Product deleted successfully'});
+    } catch (error) {
+        console.error('Error deleting ordered product', error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+exports.deleteUserProductWishlist = async (req, res) => {
+    const {email, productId} = req.params;
+    const query = {email: email, _id: (productId)};
+
+    const result = await getCollection('userProductWishlist').deleteOne(query);
+
+    res.send(result);
+}
+
+exports.getUserProductWishlist = async (req, res) => {
+    const email = req.params.email;
+    const query = {email: email};
+
+    const wishlist = await getCollection('userProductWishlist').find(query).toArray();
+    res.send(wishlist);
+}
 
 // Repeat similar functions for other user-related routes
