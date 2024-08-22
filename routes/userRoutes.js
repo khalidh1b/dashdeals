f79ad59783cc15277a4232b04130906d704cf374
Khalid <155519253+khalidh1b@users.noreply.github.com>
2024-08-22 10:27:23 +0600

    Add routes for managing user product wishlist and ordered products

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { addUserProductWishlist, getUserProductCarts, addUserProductCarts, deleteUserProductCarts} = require('../controllers/userController');
+const { addUserProductWishlist, getUserProductCarts, addUserProductCarts, deleteUserProductCarts, getUserOrderedProducts, deleteOrderedProduct, deleteUserProductWishlist, getUserProductWishlist} = require('../controllers/userController');
 
 router.post('/userProductWishlist', addUserProductWishlist);
+router.get('/getUserProductWishlist/:email', getUserProductWishlist);
+router.delete('/deleteUserProductWishlist/:email/:productId', deleteUserProductWishlist);
 router.get('/userProductCarts/:email', getUserProductCarts);
+router.get('/getUserOrderedProducts/:email', getUserOrderedProducts);
 router.post('/userProductCarts/:id/:email', addUserProductCarts);
 router.delete('/userProductCarts/:email/:id', deleteUserProductCarts);
+router.delete('/deleteOrderedProduct/:orderId/:productId', deleteOrderedProduct);
 // Add other user-related routes here
 
 module.exports = router;
