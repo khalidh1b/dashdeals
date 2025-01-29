b553c9a5d0344fe93a5fb22f1762cc3db3227fc8
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-01-29 16:25:54 +0600

    refactored new dashdealpros component

--- src/Components/DashDealPros/DashDealsPros.jsx
+++ src/Components/DashDealPros/DashDealsPros.jsx
@@ -1,30 +1,39 @@
+import services from './services.json';
+import PropTypes from 'prop-types';
 
 const DashDealsPros = () => {
     return (
-        <div className="flex gap-16 pt-28 pb-10  justify-center">
-            <div>
-                <div className="border p-2.5 rounded-full w-[70px] mx-auto bg-[#2F2E30]">
-                    <img className="bg-[#000000] rounded-full p-1" src="https://res.cloudinary.com/dksiicemx/image/upload/v1729411586/icon-delivery_qh7wv3.png" alt="delivary-icon" />
-                </div>
-                <h3 className="text-xl poppins font-semibold pt-4 text-[#000] dark:text-white">FREE AND FAST DELIVERY</h3>
-                <p className="text-[14px] poppins font-normal">Free delivery for all orders over $140</p>
-            </div>
-            <div>
-            <div className="border p-2.5 rounded-full w-[70px] mx-auto bg-[#2F2E30]">
-                <img className="bg-[#000000] rounded-full p-1" src="https://res.cloudinary.com/dksiicemx/image/upload/v1729411584/Icon-Customer_service_xrll7w.png" alt="customer-service" />
-            </div>
-            <h3 className="text-center text-xl poppins font-semibold pt-4 text-[#000] dark:text-white">24/7 CUSTOMER SERVICE</h3>
-            <p className="text-[14px] poppins font-normal text-center">Friendly 24/7 customer support</p>
-            </div>
-            <div>
-            <div className="border p-2.5 rounded-full w-[70px] mx-auto bg-[#2F2E30]">
-                <img className="bg-[#000000] rounded-full p-1" src="https://res.cloudinary.com/dksiicemx/image/upload/v1729411579/shield-tick_avmf48.svg" alt="shield-tick" />
-            </div>
-            <h3 className="text-xl poppins font-semibold pt-4 text-[#000] dark:text-white">MONEY BACK GUARANTEE</h3>
-            <p className="text-[14px] poppins font-normal text-center">We reurn money within 30 days</p>
-            </div>
+        <div className="md:flex grid gap-20 pt-28 pb-10 justify-center">
+            {services.map((service) => (
+                <ServiceItem
+                    key={service.id}
+                    icon={service.icon} 
+                    title={service.title} 
+                    description={service.description}
+                />
+            ))}
         </div>
     );
 };
 
 export default DashDealsPros;
+
+const ServiceItem = ({ icon, title, description }) => {
+    return (
+        <div>
+            <div className="border p-2.5 rounded-full w-[70px] mx-auto bg-[#2F2E30]">
+            <img className="bg-[#000000] rounded-full p-1" src={icon} alt={title} />
+            </div>
+            <h3 className="text-xl poppins font-semibold pt-4 text-[#000] dark:text-white text-center">
+                {title}
+            </h3>
+            <p className="text-[14px] poppins font-normal text-center">{description}</p>
+        </div>
+    )
+};
+
+ServiceItem.propTypes = {
+    icon: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string
+};
