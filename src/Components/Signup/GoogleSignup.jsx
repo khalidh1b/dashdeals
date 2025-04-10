38ad93b823b93e9b484b9ea2751954491683d17f
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-10 22:16:36 +0600

    feat: added conditional loader while signup

--- src/Components/Signup/GoogleSignup.jsx
+++ src/Components/Signup/GoogleSignup.jsx
@@ -1,9 +1,20 @@
 import { FcGoogle } from "react-icons/fc";
 import PropTypes from 'prop-types';
+import { Loader2 } from 'lucide-react';
 
-const GoogleSignup = ({ handleGoogleSignup }) => {
+const GoogleSignup = ({ handleGoogleSignup, loading }) => {
     return (
-        <div onClick={handleGoogleSignup} className="flex items-center gap-2 justify-center py-3 w-full mt-4 rounded text-[#000] dark:text-white poppins text-base font-normal cursor-pointer border-2"><FcGoogle className="text-2xl"/><span>Sign Up with Google</span></div>
+        <div 
+            onClick={handleGoogleSignup} 
+            className="flex items-center gap-2 justify-center py-3 w-full mt-4 rounded text-[#000] dark:text-white poppins text-base font-normal cursor-pointer border-2">
+                <span className="flex items-center gap-5">
+                    {loading 
+                        ? <Loader2 className="animate-spin"/> 
+                        : <><FcGoogle className="text-2xl"/>
+                            Sign In with Google</>
+                    }
+                </span>
+        </div>
     );
 };
 
