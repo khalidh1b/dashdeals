aa06c1b276d43ddfa31021195ba369ddedcb1825
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-10 22:40:22 +0600

    feat: added theme-toggle custom hook

---
+++ src/hooks/useToggleTheme.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useToggleTheme = () => {
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem("dashdeals-theme") === "dark";
+    });
+
+    const handleThemeChange = () => {
+        setTheme(!theme);
+        localStorage.setItem("dashdeals-theme", !theme ? "dark" : "light");
+    };
+    
+    useEffect(() => {
+        document.documentElement.classList.toggle("dark", theme);
+    }, [theme]);
+    
+    return { handleThemeChange };
+};
+
+export default useToggleTheme;
