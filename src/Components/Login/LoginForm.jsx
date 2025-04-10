cbebcd0fc3b470db1d11d00eb6fe0d17ef184658
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-10 21:56:42 +0600

    refactor: used Loader2 loader for loading spinner

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import PassResetForm from "../PassResetForm/PassResetForm";
 import PropTypes from "prop-types";
+import { Loader2 } from 'lucide-react';
 
 const LoginForm = ({ handleSignIn, handleForgetPass, loading }) => {
     return (
@@ -9,7 +10,7 @@ const LoginForm = ({ handleSignIn, handleForgetPass, loading }) => {
                     <input className="border-b-2 focus:outline-none w-full  pb-1 border-x-0 border-t-0 dark:bg-[#09090B]" type="password" name="password" id="" placeholder="Password"/>
                     <PassResetForm handleForgetPass={handleForgetPass}/>
                     <br />
-                    <button disabled={loading ? true : false} className="text-white bg-[#DB4444] rounded py-3 w-full flex justify-center gap-2" type="submit">Login{loading && <span className="loading loading-spinner loading-md"></span>}</button>
+                    <button disabled={loading ? true : false} className="text-white bg-[#DB4444] rounded py-3 w-full flex justify-center gap-2" type="submit">{loading ? <Loader2 className="animate-spin"/> : 'Login'}</button>
             </form>
         </>
     );
