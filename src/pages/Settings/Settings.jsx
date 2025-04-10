2c9f1d81e975f30debfe66853128252030cfdf9f
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-10 22:54:10 +0600

    added responsiveness for mobile devices

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -8,7 +8,7 @@ const Settings = () => {
 
     const links = [
         { to: '/settings/profile', label: 'Profile' },
-        { to: '/settings/account', label: 'Account' },
+        { to: '/settings/password&security', label: 'Password & Security' },
         { to: '/settings/appearance', label: 'Appearance' },
         { to: '/settings/notifications', label: 'Notifications' },
         { to: '/settings/display', label: 'Display' }
@@ -17,12 +17,12 @@ const Settings = () => {
     return (
         <>
         <Navbar/>
-        <div className='border-2 rounded-md px-9 py-10 h-screen'>
+        <div className='border-2 rounded-md md:px-9 px-3 py-10'>
             <h2 className='text-[#FAFAFA] text-2xl work-sans font-semibold'>Settings</h2>
             <p className='text-[#A1A1AA] text-base pb-5 font-normal'>Manage your account settings and set e-mail preferences.</p>
             <hr className='border-t mb-4 border-[#27272A]'/>
-            <div className='flex gap-8'>
-                <nav className='grid gap-2 work-sans w-3/12'>
+            <div className='md:flex items-start'>
+                <nav className='grid gap-2 mb-10 md:mb-0 work-sans w-3/12'>
                     {
                         links.map((link) => (
                             <Link key={link.label} to={link.to} className={clsx('py-2 pl-4 rounded-md font-medium', location === link.to && 'bg-[#27272A]')}>{link.label}</Link>
