f4a16c29dd732ee11ddf026b0a43f1c3d40811a6
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-11 16:24:25 +0600

    style: added gap before queryClient

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {RouterProvider} from "react-router-dom";
 import router from './Routes/Router';
 import AuthProvider from './providers/AuthProvider';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
