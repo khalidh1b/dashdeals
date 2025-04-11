276d460c9c4f97bfc9fb59c8220b7927d3e102b2
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-11 17:06:58 +0600

    feat: added loading spinner while google login

--- src/Components/Login/GoogleLogin.jsx
+++ src/Components/Login/GoogleLogin.jsx
@@ -1,9 +1,10 @@
 import { FcGoogle } from "react-icons/fc";
 import PropTypes from 'prop-types';
+import { Loader2 } from "lucide-react";
 
-const GoogleLogin = ({ handleGoogleSignIn }) => {
+const GoogleLogin = ({ handleGoogleSignIn, loading }) => {
     return (
-        <div onClick={handleGoogleSignIn} className="flex items-center gap-2 justify-center py-3 w-full mt-4 rounded text-[#000] dark:text-white poppins text-base font-normal cursor-pointer border-2"><FcGoogle className="text-2xl"/><span>Sign In with Google</span></div>
+        <div onClick={handleGoogleSignIn} className="flex items-center gap-2 justify-center py-3 w-full mt-4 rounded text-[#000] dark:text-white poppins text-base font-normal cursor-pointer border-2"><span className="flex items-center gap-5">{loading ? <Loader2 className="animate-spin"/> : <><FcGoogle className="text-2xl"/>Sign In with Google</>}</span></div>
     );
 };
 
