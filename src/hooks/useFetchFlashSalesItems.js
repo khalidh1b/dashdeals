941c4101773787585a51ed55519672d3d9e32cf7
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-11 16:15:50 +0600

    feat: added hook for fetching flash sales proudcts

---
+++ src/hooks/useFetchFlashSalesItems.js
@@ -0,0 +1,30 @@
+import useAxiosPublic from "./useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
+
+const useFetchFlashSalesItems = () => {
+    const axiosPublic = useAxiosPublic();
+    
+    const fetchProducts = async () => {
+        try {
+            const data = await axiosPublic.get('/products/flashSalesProducts')
+            return data.data;
+        } catch (error) {
+            console.error('error while fetching products', error);
+            return error;
+        }
+    };
+    
+    // useQuery to fetch flash sales products
+    const { data: products = [], isLoading, isFetching} = useQuery({
+        queryKey: ['flash_sales_products'],
+        queryFn: fetchProducts,
+    });
+    
+    return {
+        products,
+        isLoading,
+        isFetching
+    }; 
+};
+
+export default useFetchFlashSalesItems;
