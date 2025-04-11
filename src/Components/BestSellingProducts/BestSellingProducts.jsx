7c33891fefd0800564b96c1af8a7a0cf30f5b620
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-11 16:27:38 +0600

    fix: solved sizing issue in best selling proudcts headline

--- src/Components/BestSellingProducts/BestSellingProducts.jsx
+++ src/Components/BestSellingProducts/BestSellingProducts.jsx
@@ -34,7 +34,7 @@ const BestSellingProducts = () => {
             <hr className="border-t-2 mx-36 mt-16 mb-20 "/>
             <div className="flex text-[#DB4444] items-center gap-3 md:ml-36 font-semibold"><div className="bg-[#DB4444] rounded py-5 px-2"></div>This Month</div>
             <div className="flex items-center justify-between md:mx-36">
-                <div>
+                <div className="w-1/2">
                     <h2 className="text-[32px] font-semibold">Best Selling Products</h2>
                 </div>
                 <div>
