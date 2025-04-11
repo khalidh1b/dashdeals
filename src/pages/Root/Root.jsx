6236ea202cfec716741d5b9635222df859e91e85
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-11 16:39:56 +0600

    feat: added toaster for using react-hot-toast

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,10 +1,12 @@
 import Footer from '../../Components/Footer/Footer';
 import Navbar from '../../Components/Navbar/Navbar';
 import {Outlet} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 const Root = () => {
     return (
         <div>
+            <Toaster/>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
