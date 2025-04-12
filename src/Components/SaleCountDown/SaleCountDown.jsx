f0d22a5fc6dc6ad6d2935291d2189ea6805b834d
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-12 15:42:32 +0600

    refactor: added reusable time-unit component and time-units in array

--- src/Components/SaleCountDown/SaleCountDown.jsx
+++ src/Components/SaleCountDown/SaleCountDown.jsx
@@ -1,32 +1,42 @@
 import PropTypes from 'prop-types';
 
 const SaleCountDown = ({ days, hours, minutes, seconds }) => {
+    
+    const timeUnits = [
+        { label: 'Days', value: days },
+        { label: 'Hours', value: hours },
+        { label: 'Munites', value: minutes },
+        { label: 'Seconds', value: seconds },
+    ];
+    
     return (
         <div className="flex items-center gap-4 md:gap-20">
             <h2 className="md:text-[36px] text-3xl font-semibold">Flash Sales</h2>
             <div className="flex md:gap-6 gap-1">
-                <div>
-                    <p className="text-[12px] font-medium text-[#000] dark:text-white">Days</p>
-                    <span className="text-[32px] font-bold text-[#000] dark:text-white">{days < 10 ? `0${days}` : days}</span>
-                </div>
-                <div className="text-4xl text-[#DB4444] pt-4">:</div>
-                <div>
-                    <p className="text-[12px] font-medium text-[#000] dark:text-white">Hours</p>
-                    <span className="text-[32px] font-bold text-[#000] dark:text-white">{hours < 10 ? `0${hours}` : hours}</span>
-                </div>
-                <div className="text-4xl text-[#DB4444] pt-4">:</div>
-                <div>
-                    <p className="text-[12px] font-medium text-[#000] dark:text-white">Minutes</p>
-                    <span className="text-[32px] font-bold text-[#000] dark:text-white">{minutes < 10 ? `0${minutes}` : minutes}</span>
-                </div>
-                <div className="text-4xl text-[#DB4444] pt-4">:</div>
-                <div>
-                    <p className="text-[12px] font-medium text-[#000] dark:text-white">Seconds</p>
-                    <span className="text-[32px] font-bold text-[#000] dark:text-white">{seconds < 10 ? `0${seconds}` : seconds}</span>
-                </div>
+                {timeUnits.map((unit, index) => (
+                    <div key={unit.label} className='flex items-center gap-1'>
+                        <TimeUnit label={unit.label} value={unit.value}/>
+                        {index !== timeUnits.length - 1 && (
+                            <div className='text-4xl text-[#DB4444] pt-4'>:</div>
+                        )}
+                    </div>
+                ))}
             </div>
         </div>
     );
+};
+
+export default SaleCountDown;
+
+const TimeUnit = ({ label, value }) => {
+    return (
+        <div className="text-center">
+            <p className="text-[12px] font-medium text-[#000] dark:text-white">{label}</p>
+            <span className="text-[32px] font-bold text-[#000] dark:text-white">
+                {value < 10 ? `0${value}` : value}
+            </span>
+        </div>
+    )
 };
 
 SaleCountDown.propTypes = {
@@ -36,4 +46,7 @@ SaleCountDown.propTypes = {
     seconds: PropTypes.number.isRequired,
 };
 
-export default SaleCountDown;
+TimeUnit.propTypes = {
+    label: PropTypes.string,
+    value: PropTypes.number
+};
