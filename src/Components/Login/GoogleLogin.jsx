cb914d342770aab3ae3851e74cba4a01828da431
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-12 13:02:59 +0600

    style: formatted component in a good manner

--- src/Components/Login/GoogleLogin.jsx
+++ src/Components/Login/GoogleLogin.jsx
@@ -4,12 +4,23 @@ import { Loader2 } from "lucide-react";
 
 const GoogleLogin = ({ handleGoogleSignIn, loading }) => {
     return (
-        <div onClick={handleGoogleSignIn} className="flex items-center gap-2 justify-center py-3 w-full mt-4 rounded text-[#000] dark:text-white poppins text-base font-normal cursor-pointer border-2"><span className="flex items-center gap-5">{loading ? <Loader2 className="animate-spin"/> : <><FcGoogle className="text-2xl"/>Sign In with Google</>}</span></div>
+        <div 
+            onClick={handleGoogleSignIn} 
+            className="flex items-center gap-2 justify-center py-3 w-full mt-4 rounded text-[#000] dark:text-white poppins text-base font-normal cursor-pointer border-2"
+        >
+            <span className="flex items-center gap-5">
+                    {loading 
+                        ? <Loader2 className="animate-spin"/> 
+                        : <><FcGoogle className="text-2xl"/>Sign In with Google</>
+                    }
+            </span>
+        </div>
     );
 };
 
 GoogleLogin.propTypes = {
     handleGoogleSignIn: PropTypes.func,
+    loading: PropTypes.func
 }
 
 export default GoogleLogin;
