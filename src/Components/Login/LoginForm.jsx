50935650354b857c7f4ba78acd2e9e44a341970e
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-12 12:59:25 +0600

    style: formatted component in a good manner

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -6,11 +6,30 @@ const LoginForm = ({ handleSignIn, handleForgetPass, loading }) => {
     return (
         <>
             <form onSubmit={handleSignIn}>
-                    <input className="focus:outline-none border-b-2 w-full pb-1 mb-7 border-x-0 border-t-0 dark:bg-[#09090B]" type="email" name="email" id="" placeholder="Email or Phone Number"/> <br />
-                    <input className="border-b-2 focus:outline-none w-full  pb-1 border-x-0 border-t-0 dark:bg-[#09090B]" type="password" name="password" id="" placeholder="Password"/>
+                    <input 
+                        className="focus:outline-none border-b-2 w-full pb-1 mb-7 border-x-0 border-t-0 dark:bg-[#09090B]" 
+                        type="email" 
+                        name="email" 
+                        id="" 
+                        placeholder="Email or Phone Number"
+                    /> 
+                    <br />
+                    <input 
+                        className="border-b-2 focus:outline-none w-full  pb-1 border-x-0 border-t-0 dark:bg-[#09090B]" 
+                        type="password" 
+                        name="password" 
+                        id="" 
+                        placeholder="Password"
+                    />
                     <PassResetForm handleForgetPass={handleForgetPass}/>
                     <br />
-                    <button disabled={loading ? true : false} className="text-white bg-[#DB4444] rounded py-3 w-full flex justify-center gap-2" type="submit">{loading ? <Loader2 className="animate-spin"/> : 'Login'}</button>
+                    <button 
+                        disabled={loading ? true : false} 
+                        className="text-white bg-[#DB4444] rounded py-3 w-full flex justify-center gap-2" 
+                        type="submit"
+                    >
+                        {loading ? <Loader2 className="animate-spin"/> : 'Login'}
+                    </button>
             </form>
         </>
     );
