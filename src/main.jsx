4c0f86069e4e88f829b7c76ab55518eafbfc37a9
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-27 17:43:33 +0600

    feat: integrated stripe elements & configuration with react app

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,20 @@ import {RouterProvider} from "react-router-dom";
 import router from './Routes/Router';
 import AuthProvider from './providers/AuthProvider';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 const queryClient = new QueryClient();
+
+const stripePromise = loadStripe(import.meta.env.VITE_Stripe_PK);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
         <AuthProvider>
           <QueryClientProvider client={queryClient}>
-                <RouterProvider router={router} />
+                <Elements stripe={stripePromise}>                  
+                  <RouterProvider router={router} />
+                </Elements>
           </QueryClientProvider>
         </AuthProvider>
   </React.StrictMode>,
