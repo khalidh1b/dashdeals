a88add0861a93d58a18cfbd93a50b1bd1c756413
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-27 19:05:27 +0600

    refactor: removed successpayment, failedpayment, cancelpayment routes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { createPayment, handleSuccessPayment, handlePaymentFailed, handlePaymentCancel } = require('../controllers/paymentController');
+const { createPayment } = require('../controllers/paymentController');
 
 router.post('/create-payment', createPayment);
-router.post('/success-payment', handleSuccessPayment);
-router.post('/payment-failed', handlePaymentFailed);
-router.post('/payment-cancel', handlePaymentCancel);
-
-// Add other payment-related routes here
 
 module.exports = router;
