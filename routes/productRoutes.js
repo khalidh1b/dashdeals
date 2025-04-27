fecfb73c34c5f59457ae5b43364270799a9dd5d2
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-27 19:07:53 +0600

    feat: added getSearchproducts route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getFlashSalesProducts, getBestSellingProducts, getExploreOurProducts, getFlashSalesProductById } = require('../controllers/productController');
+const { 
+    getFlashSalesProducts, 
+    getBestSellingProducts, 
+    getExploreOurProducts, 
+    getFlashSalesProductById,
+    getSearchedProducts 
+} = require('../controllers/productController');
 
 router.get('/flashSalesProducts', getFlashSalesProducts);
 router.get('/bestSellingProducts', getBestSellingProducts);
 router.get('/exploreOurProducts', getExploreOurProducts);
 router.get('/flashSalesProducts/:id', getFlashSalesProductById);
+router.get('/searchProducts/:searchString', getSearchedProducts);
 
 // Add other product-related routes here
 
