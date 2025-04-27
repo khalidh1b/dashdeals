b8dff31e6ed4e93bfd2e06206ed6f2d778ef250e
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-27 18:31:24 +0600

    refactor: removed unused params

--- src/lib/getPaymentSuccessParams.js
+++ src/lib/getPaymentSuccessParams.js
@@ -1,12 +1,9 @@
 import { getUrlParameter } from "./getUrlParameter";
 
 export const getPaymentSuccessParams = () => {
-    // Usage
+
     const tran_id = getUrlParameter('tran_id');
-    const card_issuer = getUrlParameter('card_issuer');
-    const tran_date = getUrlParameter('tran_date');
-    const currency_type = getUrlParameter('currency_type');
     const amount = getUrlParameter('amount');
 
-    return { tran_id, card_issuer, tran_date, currency_type, amount }; 
+    return { tran_id, amount }; 
 };
