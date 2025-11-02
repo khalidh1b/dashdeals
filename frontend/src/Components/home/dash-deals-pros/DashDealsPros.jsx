3cc4e5d2e05bb8472d87d4872da8fb10a199c59b
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-02 10:52:00 +0600

    perf(ui): Add explicit dimensions to ServiceItem icons

--- frontend/src/Components/home/dash-deals-pros/DashDealsPros.jsx
+++ frontend/src/Components/home/dash-deals-pros/DashDealsPros.jsx
@@ -23,7 +23,7 @@ const ServiceItem = ({ icon, title, description }) => {
     return (
         <div>
             <div className="border p-2.5 rounded-full w-[70px] mx-auto bg-[#2F2E30]">
-            <Image className="bg-[#000000] rounded-full p-1" src={icon} alt={title} />
+            <Image className="bg-[#000000] rounded-full p-1" width={50} height={50} src={icon} alt={title} />
             </div>
             <h3 className="text-xl poppins font-semibold pt-4 text-black dark:text-white text-center">
                 {title}
