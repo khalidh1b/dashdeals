e5133efabc3ea8d2f0a26c955886280dc4833ec5
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-08 00:38:23 +0600

    feat(hooks): Create index file for order management hooks
    Introduces `index.js` to simplify imports for related order management custom hooks:
    - `useFetchOrdersData`
    - `useDeleteOrder`
    - `useOrdersLogic`

---
+++ frontend/src/features/orders/hooks/index.js
@@ -0,0 +1,3 @@
+export { default as useFetchOrdersData } from './useFetchOrdersData';
+export { default as useDeleteOrder } from './useDeleteOrder';
+export { useOrdersLogic } from './useOrdersLogic';
