8100f2c187021cf86ea59770d5775e7e4528e542
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-19 16:11:36 +0600

    refactor: wishlists page and its component added, folder renamed to small case wishlist

---
+++ frontend/src/pages/wishlist/Wishlists.jsx
@@ -0,0 +1,34 @@
+import useHandleCart from "@/features/cart/hooks/useHandleCart";
+import useHandleDeleteWishlist from "@/features/user/hooks/useHandleDeleteWishlist";
+import useFetchWishlist from "@/features/user/hooks/useFetchWishlist";
+import Wishlist from "@/pages/wishlist/Wishlist";
+import WishlistSkeleton from '@/pages/wishlist/wishlist-skeleton/WishlistSkeleton';
+import { ProductCard } from "@/components/common/product-card/ProductCard";
+import { EmptyState } from "@/shared/components/empty-states";
+
+const Wishlists = () => {
+    const [products, , isLoading] = useFetchWishlist();
+    const handleDelete = useHandleDeleteWishlist();
+    const handleCart = useHandleCart();
+
+    return (
+        <div className="pt-20 pb-28">
+            {products.length !== 0 && 
+            <div className="flex md:justify-between justify-evenly items-center md:mx-32">
+                <p>Wishlist<span> ({products.length})</span></p>
+            </div>
+            }
+
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-8 pt-10 md:mx-32">
+                {   isLoading && <WishlistSkeleton/>}
+
+                {products.length === 0 
+                ? <EmptyState type={'wishlist'}/>
+                : products.map(product => (<Wishlist key={product._id} product={product} handleDelete={handleDelete} handleCart={handleCart}/>))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Wishlists;
