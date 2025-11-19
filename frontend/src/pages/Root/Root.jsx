4589ca8e1791827336d95d01b1128098a060f7a6
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-19 12:54:53 +0600

    refactor: update Root for dark mode compatibility

--- frontend/src/pages/Root/Root.jsx
+++ frontend/src/pages/Root/Root.jsx
@@ -1,6 +1,6 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { LoadingSkeleton } from '@/components/common/skeletons/loading-skeleton';
 
 // Lazy load layout component
@@ -8,8 +8,14 @@ const Navbar = lazy(() => import('@/components/common/navbar/Navbar'));
 const Footer = lazy(() => import('@/components/common/footer/Footer'));
 
 const Root = () => {
+    // useEffect(() => {
+    //    toast("⚠️ We're currently fixing some major bugs. Thanks for your patience!", {
+    //       duration: 100000000,
+    //     });
+    // }, []);
+
     return (
-        <div>
+        <div className='bg-muted/70 dark:bg-gray-700'>
             <Toaster/>
             <Suspense fallback={<LoadingSkeleton />}>
                 <Navbar/>
