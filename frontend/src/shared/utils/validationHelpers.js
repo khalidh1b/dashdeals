dd92d89c7344f5d35637e0825bdc01b42ad541d9
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-19 12:54:11 +0600

    refactor: validation exports - utility file to re-export validation functions

---
+++ frontend/src/shared/utils/validationHelpers.js
@@ -0,0 +1,15 @@
+export {
+  validatecartitemid,
+  validateproductid,
+  validateproductobject,
+  validateuserauth,
+  validatedeletecartoperation,
+  logvalidationerror,
+
+  validateCartItemId,
+  validateProductId,
+  validateProductObject,
+  validateUserAuth,
+  validateDeleteCartOperation,
+  logValidationError
+} from './validation/index.js';
