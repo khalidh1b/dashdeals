7c1e04d90c312d72629ed2424055f2a68b572fe3
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-19 12:43:31 +0600

    refactor: update WishlistSkeleton for dark mode compatibility

--- frontend/src/pages/Wishlist/wishlist-skeleton/WishlistSkeleton.jsx
+++ frontend/src/pages/Wishlist/wishlist-skeleton/WishlistSkeleton.jsx
@@ -8,19 +8,19 @@ const WishlistSkeleton = () => {
             <React.Fragment key={idx}>
                 <div>
                     <div className="relative">
-                        <Skeleton className="bg-[#F5F5F5] w-[300px] h-[300px] rounded" />
-                        <span className="bg-[#4e4c4c] absolute top-5 left-4 text-white py-1 px-4 rounded poppins">
+                        <Skeleton className="bg-muted dark:bg-gray-800 w-[300px] h-[300px] rounded" />
+                        <span className="bg-muted-foreground absolute top-5 left-4 text-white py-1 px-4 rounded poppins">
                         <Skeleton className="w-[50px] h-[20px]" />
                         </span>
-                        <Skeleton className="bg-[#FFFFFF] dark:text-red-600 absolute top-3 left-60 w-[45px] h-[45px] rounded-full" />
-                        <Skeleton className="bg-[#000000] absolute bottom-0 w-[300px] h-[50px] rounded-b" />
+                        <Skeleton className="bg-background dark:bg-gray-700 absolute top-3 left-60 w-[45px] h-[45px] rounded-full" />
+                        <Skeleton className="bg-foreground dark:bg-gray-900 absolute bottom-0 w-[300px] h-[50px] rounded-b" />
                     </div>
                     
-                    <Skeleton className="bg-[#000000] w-[250px] h-[20px] rounded mt-3" />
+                    <Skeleton className="bg-foreground dark:bg-gray-900 w-[250px] h-[20px] rounded mt-3" />
 
                     <div className="flex gap-4 py-2">
-                        <Skeleton className="bg-[#DB4444] w-[100px] h-[20px] rounded" />                
-                        <Skeleton className="bg-gray-500 w-[100px] h-[20px] rounded line-through" />
+                        <Skeleton className="bg-red-600 w-[100px] h-[20px] rounded" />                
+                        <Skeleton className="bg-muted-foreground w-[100px] h-[20px] rounded line-through" />
                     </div>
                 </div>
             </React.Fragment>
